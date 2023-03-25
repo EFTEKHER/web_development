acffd78719107c10f8cd74aef171d41f4796e20b
EFTEKHER <eftekherali2000@gmail.com>
2023-03-25 16:04:45 +0600

    y

---
+++ javascript/chapter3-strings/main.js
@@ -0,0 +1,18 @@
+const myVariable="Mathematics";
+
+//the length;
+
+console.log(myVariable.length);
+
+
+console.log("My name is Eftekher Ali Efte".length);
+
+console.log(myVariable.charAt(0));
+
+console.log(myVariable.indexOf("Mat"));
+console.log(myVariable.lastIndexOf("ath"));
+console.log(myVariable.slice(5,8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.split(""));
+console.log("john,joe,david".split(","))
